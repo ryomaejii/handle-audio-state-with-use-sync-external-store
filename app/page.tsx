7b1e79348de0f904d69aefee0aa327b8f6473c54
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2024-08-01 22:55:52 +0900

    feat: Import Audio Player in Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import AudioPlayer from "./components/AudioPlayer";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+    >
+      <AudioPlayer src="sample.mp3" />
+      <AudioPlayer src="sample.mp3" />
+      <AudioPlayer src="sample.mp3" />
+    </div>
+  );
 }
